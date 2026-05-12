3e3b38ed989339eba1e039f1611f7f0d8cd0e29d
zuntes <dung.pt152001@outlook.com>
2026-05-12 11:16:29 +0700

    Fix launch file for controller pkg

--- drone_controller/setup.py
+++ drone_controller/setup.py
@@ -12,6 +12,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch',
             ['launch/controller.launch.py']),
+        ('share/' + package_name + '/launch',
+            ['launch/controller_sim.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
